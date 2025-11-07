2b1b59913be6622e30f85d1f04c2a8ef64406ed4
ajayraj39 <ajay.23b1531196@abes.ac.in>
2025-11-07 09:57:54 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+function greet(name, callback) {
+  console.log("Hello " + name);
+    callback();
+}
+
+function sayGoodbye() {
+  console.log("Goodbye!");
+}
+ 
+// Passing sayGoodbye as a callback
+greet("Ashutosh", sayGoodbye);
